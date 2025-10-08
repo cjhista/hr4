38505b8864058a0e1d155943be92f885c465294a
cjhista <histajames123@gmail.com>
2025-10-08 23:04:30 +0800

    1

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 // db.php - Database Connection File
 
-$host = "localhost:5507";   // MySQL host (karaniwan: localhost o 127.0.0.1)
+$host = "localhost:3307";   // MySQL host (karaniwan: localhost o 127.0.0.1)
 $user = "root";        // MySQL username (default sa XAMPP/MAMP ay root)
 $pass = "";            // MySQL password (lagyan kung meron)
 $db   = "hr4_db";      // Database name
