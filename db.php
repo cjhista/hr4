98846a656cdbdf595d35412ed93ab61893dca6a3
cjhista <histajames123@gmail.com>
2025-10-13 17:22:23 +0800

    1

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";  // CyberPanel uses localhost for MySQL
-$username   = "hr4_user";
-$password   = "hr412345";
+$username   = "hr4_hr4_user";
+$password   = "hr4123";
 $dbname     = "hr4_hr4_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
