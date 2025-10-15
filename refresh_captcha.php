c237b7a979325b322898385d6ce635cdd3f74ade
cjhista <histajames123@gmail.com>
2025-10-15 19:24:30 +0800

    1

--- refresh_captcha.php
+++ refresh_captcha.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Generate new CAPTCHA code
-$captcha_code = substr(str_shuffle("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 6);
+$captcha_code = substr(str_shuffle("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"), 0, 6);
 $_SESSION['captcha_code'] = $captcha_code;
 
 echo json_encode(['success' => true, 'captcha_code' => $captcha_code]);
